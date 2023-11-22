52d84a06ecd660cccf062915e52db0172a1305f1
endeos-pmonclus <pmonclus@endeos.com>
2023-11-22 11:53:24 +0100

    [UPD] endeos rest api | finish create invoice controller

--- endeos_rest_api/__manifest__.py
+++ endeos_rest_api/__manifest__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Services/Project",
-    "version": "16.0.1.0.5",
+    "version": "16.0.1.0.6",
 
     # any module necessary for this one to work correctly
-    "depends": ["base", "sale", "purchase", "stock"],
+    "depends": ["sale", "purchase", "stock"],
 
     # always loaded
     "data": [],
