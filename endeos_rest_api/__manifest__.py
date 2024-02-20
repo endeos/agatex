c937a54cee070681141b386217b8612dd9f36d34
endeos-pmonclus <pmonclus@endeos.com>
2024-02-20 13:52:03 +0100

    [UPD] endeos rest api | sale controller refactor method to create new so

--- endeos_rest_api/__manifest__.py
+++ endeos_rest_api/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Services/Project",
-    "version": "16.0.1.0.11",
+    "version": "16.0.1.0.12",
 
     # any module necessary for this one to work correctly
     "depends": ["sale", "purchase", "stock"],
