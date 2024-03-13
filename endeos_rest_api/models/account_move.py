26978c128d4f28dd1ddb0c5fad3c706b22e7821f
endeos-shernandez <shernandez@endeos.com>
2024-03-13 11:38:36 +0100

    [UPD] resst_api | New field on sale orders

--- endeos_rest_api/models/account_move.py
+++ endeos_rest_api/models/account_move.py
@@ -5,3 +5,10 @@ class AccountMove(models.Model):
 
     x_sale_incoterm_location = fields.Char(string="Ubicación del Incoterm", help="Valor recogido de la venta si se ha creado a través de la API")
     x_sale_incotrastat_transport_code = fields.Char(string="Código Transporte Incotrastat", help="Valor recogido de la venta si se ha creado a través de la API")
+    
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    x_sale_line_merc_code = fields.Char(
+        string="Código de mercancia", 
+        help="Valor recogido del producto asociado si se ha creado a través de la API")
