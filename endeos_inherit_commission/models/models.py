10bda99a1dc504fe6705a80f1788eadd713946fc
endeos-shernandez <shernandez@endeos.com>
2024-03-20 11:14:30 +0100

    [UPD] inherit_commission | relation with commissions

--- endeos_inherit_commission/models/models.py
+++ endeos_inherit_commission/models/models.py
@@ -1,53 +1,4 @@
-# # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
-# from datetime import datetime
-# import logging
-
-# _logger = logging.getLogger(__name__)
-    
-
-# class CommissionSettleLine(models.Model):
-#     _inherit = 'commission.settlement.line'
-    
-#     commission_ids = fields.Many2many(
-#         comodel_name="commission",
-#         string="Commissions",
-#         copy=True
-#     )
-    
-# class SaleOrderLineAgent(models.Model):
-#     _inherit = "sale.order.line.agent"
-    
-#     @api.depends(
-#         "commission_ids",
-#         "object_id.price_subtotal",
-#         "object_id.product_id",
-#         "object_id.product_uom_qty",
-#     )
-#     def _compute_amount(self):
-#         for line in self:
-#             order_line = line.object_id
-#             line.amount = line._get_commission_amount(
-#                 line.commission_ids,
-#                 order_line.price_subtotal,
-#                 order_line.product_id,
-#                 order_line.product_uom_qty,
-#             )
-            
-# class CommissionLineMixin(models.AbstractModel):
-#     _inherit = "commission.line.mixin"
-
-#     commission_ids = fields.Many2many(
-#             comodel_name="commission",
-#             string="Commissions",
-#             compute="_compute_commission_id",
-#             copy=True
-#         )
-#     @api.depends("agent_id")
-#     def _compute_commission_id(self):
-#         for record in self:
-#             record.commission_ids = record.agent_id.commission_id
 from odoo import api, fields, models
 
 class SaleOrderLineAgent(models.Model):
@@ -55,7 +6,7 @@ class SaleOrderLineAgent(models.Model):
 
     # Nuevo campo para seleccionar una comisión entre todas las disponibles
     all_commission_id = fields.Many2one(
-        'sale.commission', 
+        'commission', 
         string="All Commissions"
     )
 
