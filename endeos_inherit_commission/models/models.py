6f0a052d35803ce0a51de7a423b8f35af3c323bd
endeos-shernandez <shernandez@endeos.com>
2024-03-20 09:57:29 +0100

    [UPD] inherit_commission | changes

--- endeos_inherit_commission/models/models.py
+++ endeos_inherit_commission/models/models.py
@@ -10,8 +10,28 @@ _logger = logging.getLogger(__name__)
 class CommissionSettleLine(models.Model):
     _inherit = 'commission.settlement.line'
     
-commission_ids = fields.Many2many(
-    comodel_name="commission",
-    string="Commissions",
-    copy=True
-)
+    commission_ids = fields.Many2many(
+        comodel_name="commission",
+        string="Commissions",
+        copy=True
+    )
+    
+class SaleOrderLineAgent(models.Model):
+    _inherit = "sale.order.line.agent"
+
+    @api.depends(
+        "commission_id",
+        "commission_ids",
+        "object_id.price_subtotal",
+        "object_id.product_id",
+        "object_id.product_uom_qty",
+    )
+    def _compute_amount(self):
+        for line in self:
+            order_line = line.object_id
+            line.amount = line._get_commission_amount(
+                line.commission_id,
+                order_line.price_subtotal,
+                order_line.product_id,
+                order_line.product_uom_qty,
+            )
