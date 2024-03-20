79d8ae3532227f22672d0ba7d49a720ee0090be3
endeos-shernandez <shernandez@endeos.com>
2024-03-20 11:30:21 +0100

    [UPD] inherit_commission | restriction

--- endeos_inherit_commission/models/models.py
+++ endeos_inherit_commission/models/models.py
@@ -6,10 +6,15 @@ class SaleOrderLineAgent(models.Model):
 
     # Nuevo campo para seleccionar una comisión entre todas las disponibles
     all_commission_id = fields.Many2one(
-        'commission', 
+        'sale.commission', 
         string="All Commissions"
     )
 
+    @api.onchange('all_commission_id')
+    def _onchange_all_commission_id(self):
+        for record in self:
+            record.commission_id = record.all_commission_id
+            
     @api.depends(
         "all_commission_id",  # Cambiar a la nueva comisión seleccionada
         "object_id.price_subtotal",
@@ -26,12 +31,4 @@ class SaleOrderLineAgent(models.Model):
                 order_line.product_id,
                 order_line.product_uom_qty,
             )
-
-class SaleOrderLineAgent(models.Model):
-    _inherit = "commission.line.mixin"
-    
-    @api.onchange('all_commission_id')
-    def _onchange_all_commission_id(self):
-        for record in self:
-            record.commission_id = record.all_commission_id
-    
+            
